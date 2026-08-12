9fcbfbce6d15d1a57eee434a562a7fc176ae6aaa
Ronel <ronelmelendrez01@gmail.com>
2026-08-12 23:58:04 +0800

    fixed: the modal function for add stock for each product

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function openStockModal() {
 function closeStockModal() {
     document.getElementById('stockModal').style.display = 'none';
 }
+
+function addStock() {
+    const product = document.getElementById('productSelect').value;
+    const quantity = document.getElementById('stockQuantity').value;
+    
+    if(!product){
+        alert('Please enter a product name.');
+        return;
+    }
+
+    if(!quantity || quantity <= 0){
+        alert('Please enter a valid quantity.');
+        return;
+    }
+
+
+console.log("Product:", product);
+console.log("Quantity:", quantity);
+
+ closeStockModal();
+
+ document.getElementById('productSelect').value = '';
+ document.getElementById('stockQuantity').value = '';
+}
