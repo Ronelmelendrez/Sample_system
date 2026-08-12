ef216adfb15512b4a93df5d1e7cc48baba04fcdf
Ronel <ronelmelendrez01@gmail.com>
2026-08-12 23:37:34 +0800

    feat: add a function for js to flex and close the stock modal form

---
+++ script.js
@@ -0,0 +1,7 @@
+function openStockModal() {
+    document.getElementById('stockModal').style.display = 'flex';
+}
+
+function closeStockModal() {
+    document.getElementById('stockModal').style.display = 'none';
+}
